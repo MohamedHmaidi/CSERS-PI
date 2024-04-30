755e5198da71bfca35482cc63ea92219a924c27b
m0rphMZ <mahmoud.mzoughi@esprit.tn>
2024-04-30 19:02:38 +0100

    Revert "training content push"
    This reverts commit 6db78726113690a9a570df297c70ae81c4eff6ea.

--- pitraining/src/main/java/tn/esprit/pitraining/repositories/TrainingContentRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.pitraining.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.pitraining.entities.TrainingContent;
-
-public interface TrainingContentRepository extends JpaRepository<TrainingContent, Long> {
-}
