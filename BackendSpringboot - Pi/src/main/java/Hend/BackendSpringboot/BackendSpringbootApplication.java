1561b6d88521d1c709ded223ef0d6fca04d564b2
istabrakzouabi12 <istabrak.zouabi@esprit.tn>
2024-05-02 11:41:35 +0100

    pi

---
+++ BackendSpringboot - Pi/src/main/java/Hend/BackendSpringboot/BackendSpringbootApplication.java
@@ -0,0 +1,15 @@
+package Hend.BackendSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class BackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringbootApplication.class, args);
+	}
+
+}
